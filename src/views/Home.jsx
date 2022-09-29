f6fdfd7037804c43dafc9a67c569a3121a453f8b
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2022-09-29 17:53:58 -0300

    Template

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -18,19 +18,34 @@ const Home = () => {
   const { setUser } = useAuth();
 
   const doLogout = () => {
-    setUser({
-      id: "",
-      name: "",
-      token: ""
-    });
+    setUser({ token: "" });
 
     localStorage.removeItem("user");
   }
 
   return (
-    <motion.div initial={framer.initial} animate={framer.animate} exit={framer.exit}>
-      <h1 className="u-h1">Home</h1>
-      <button onClick={doLogout}>Sair</button>
+    <motion.div className="app" initial={framer.initial} animate={framer.animate} exit={framer.exit}>
+      <div className="app__wrapper">
+        <div className="app__menu">
+          <div className="app__header app__header--bg-02">
+            <h2 className="heading-sm">Channels</h2>
+          </div>
+          <div className="app__menu-body">
+            <div className="x-p-20 y-p-20">
+              Dentro do menu
+            </div>
+          </div>
+        </div>
+
+        <div className="app__container app__header--bg-03">
+          <div className="app__header">
+            <h2 className="p-sm fw-5">Tecnologia</h2>
+            <div className="x-p-20 y-p-20">
+              <button onClick={doLogout}>Fazer logout</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </motion.div>
   )
 }
